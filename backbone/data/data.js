4ada6d013f4c8d312d0cec0afc825530904050e4
John Kinney <johnkinney77@gmail.com>
2015-07-12 15:10:32 -0400

    hero image done with subnav completed

---
+++ backbone/data/data.js
@@ -0,0 +1,10 @@
+[
+{name: 'Sam Soffes',
+comment: 'How to Get Inspired: the Right Way - Designmodo bit.ly/1hqgbQA Good stuff from <a class="at-someone" href="#">@designmodo!</a>',
+img_url: "./public/imgs/profile_pictures/sam-soffes-profile-pic.jpg"},
+{name: 'Meg Robichaud',
+comment: 'My view this morning is simply beautiful... instagram.com/p/mV0PUrHRwQ/',
+img_url: "./public/imgs/profile_pictures/meg-robinchaud-profile-pic.jpg",
+photo_url: "./public/imgs/meg-instagram.jpg"
+}
+]
